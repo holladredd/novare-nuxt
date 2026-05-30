6ee4801134efdd21e6ee5e499ed8e9658ad1d4a9
olamide folayan <holladred@gmail.com>
2026-05-31 00:17:50 +0100

    feat: implement global dark mode configuration and base styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   future: {
@@ -8,5 +7,12 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
   ],
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: {
+        class: 'dark'
+      }
+    }
+  }
 })
